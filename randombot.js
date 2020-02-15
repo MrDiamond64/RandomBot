fe1c74b0b263628cebb6bc731ddeb6ff86afec2d
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2020-02-15 14:06:05 -0600

    Add dotenv plugin

--- randombot.js
+++ randombot.js
@@ -13,6 +13,12 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
 
+// .env stuff
+const { config } = require("dotenv");
+config({
+    path: __dirname + "/.env"
+})
+
 // Command Files Search
 const commandFiles = fs
   .readdirSync("./commands")
